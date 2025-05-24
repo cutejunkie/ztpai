f4aff4d03f195ae8f5807bea600f40d0a116f727
cutejunkie <obrzud.gabriela@gmail.com>
2025-05-24 21:59:12 +0200

    refactor: now buttons work and route to other pages

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,15 +1,17 @@
+import { Link } from "react-router-dom";
+
 function Sidebar() {
-    return (
-      <div className="Sidebar">
-        <img className="Sidebar-logo" src="src/assets/logo.jpg" alt="logo"></img>
-        <div className="Sidebar-buttons">
-          <button>main page</button>
-          <button>profile</button>
-          <button>add person</button>
-          <button>favourites</button>
-        </div>
+  return (
+    <div className="Sidebar">
+      <img className="Sidebar-logo" src="src/assets/logo.jpg" alt="logo" />
+      <div className="Sidebar-buttons">
+        <Link to="/"><button>main page</button></Link>
+        <Link to="/profile"><button>profile</button></Link>
+        <Link to="/add"><button>add person</button></Link>
+        <Link to="/favourites"><button>favourites</button></Link>
       </div>
-    );
+    </div>
+  );
 }
 
 export default Sidebar;
