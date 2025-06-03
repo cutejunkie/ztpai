6378de5289174ec1b23b0d44db6479325ea11f43
cutejunkie <obrzud.gabriela@gmail.com>
2025-06-04 00:16:55 +0200

    refactor: CardSerializer updated

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -4,4 +4,5 @@ from .models import Card
 class CardSerializer(serializers.ModelSerializer):
     class Meta:
         model = Card
-        fields = ['uuid', 'title', 'content']  # user nie musi być tutaj – przypisujemy go w widoku
+        fields = ['uuid', 'title', 'content', 'birth_date', 'favourite']
+        read_only_fields = ['uuid', 'user']
