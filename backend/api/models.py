7017056dc7c1a0ed01861b09d985651c67256f9d
cutejunkie <obrzud.gabriela@gmail.com>
2025-06-03 21:50:10 +0200

    refactor: default user.id for cards is 1 (me)

--- backend/api/models.py
+++ backend/api/models.py
@@ -15,4 +15,6 @@ class Card(models.Model):
     uuid = models.UUIDField(default=uuid.uuid4, unique=True, editable=False)
     title = models.CharField(max_length=200)
     content = models.TextField()
-    user = models.ForeignKey('CustomUser', on_delete=models.CASCADE, related_name='cards')
+    # user = models.ForeignKey('CustomUser', on_delete=models.CASCADE, related_name='cards')
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, default=1)  # ja xd
+
